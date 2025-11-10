e428d8b586ab24c35bd0389d7c55d434da6bc3c8
kaka245 <andikacaesario07@gmail.com>
2025-11-10 20:17:44 +0700

    Add files via upload

---
+++ belajar lagi.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout<<"kaka"<<endl;
+	cout<<"kamu"<<endl;
+	cout<<"suka"<<endl;
+	cout<<"makan"<<endl;
+	cout<<"apa?"<<endl;
+	return 0;
+}
